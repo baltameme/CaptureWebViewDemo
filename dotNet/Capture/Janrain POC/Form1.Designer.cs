0c9e8e5c45841dd06e82eb36f1939933b53389ef
Chad <cmurphy8689@gmail.com>
2012-03-07 14:46:22 -0800

    POC for .NET

---
+++ dotNet/Capture/Janrain POC/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.loading = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(388, 575);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // loading
+            // 
+            this.loading.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
+            this.loading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loading.Location = new System.Drawing.Point(23, 13);
+            this.loading.Name = "loading";
+            this.loading.Size = new System.Drawing.Size(326, 47);
+            this.loading.TabIndex = 1;
+            this.loading.Text = "Please wait while the capture widget loads...";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.statusLabel.AutoEllipsis = true;
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.BackColor = System.Drawing.SystemColors.Info;
+            this.statusLabel.Location = new System.Drawing.Point(111, 561);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(41, 13);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "loading";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 561);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Status: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 575);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.loading);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "POC - Capture Registration and Login from .NET App";
+            this.Load += new System.EventHandler(this.loadHandler);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loading;
+        public System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ dotNet/Capture/Janrain POC/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public partial class Form1 : Form
+    {
+        String captureServer = "https://webview-poc.dev.janraincapture.com";
+        String redirectUri = "http://10.0.0.96/~chad/intelCapture.html";
+        String clientId = "zc7tx83fqy68mper69mxbt5dfvd7c2jh";
+        String responseType = "token";
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Navigate to a web page that has the custome widget embedded.
+            webBrowser1.Navigate(new System.Uri(captureServer + "/oauth/signin_mobile?redirect_uri=" + redirectUri + "&client_id=" + clientId + "&response_type=" + responseType));
+        }
+
+        // Can use window.external.setProfileName(profileName) to call this method from JavaScript.
+        public void setProfileName(string profileName)
+        {
+            Console.WriteLine(profileName); 
+        }
+
+        public void getProfile(string token)
+        {
+            string url = "https://webview-poc.dev.janraincapture.com/entity?access_token=" + token;
+            WebRequest request = WebRequest.Create(url);
+
+            request.ContentType = "text/html";
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            // Resize a few items... 
+            Form1.ActiveForm.Height = 500;
+            Form1.ActiveForm.Width = 940;
+            loading.Height = 444;
+            loading.Width = 889;
+            loading.Text = responseFromServer;
+            loading.Show();
+
+            // Hide the webBrowser
+            webBrowser1.Hide();
+        }
+
+        // Can use window.external.setStatus(status) to call this method from JavaScript.
+        public void setStatus(string text)
+        {
+            statusLabel.Text = text;
+        }
+
+        // This is called from the janrainWidgetOnload function.
+        public void hideLoadingLabel()
+        {
+            loading.Hide();
+        }
+
+        public void setHeight(int height)
+        {
+            // Adding a little bit of padding as these numbers are not ideal.
+            if (height > 0) this.Height = height + 30;
+        }
+
+        public void setWidth(int width)
+        {
+            // Adding padding...
+            if (width > 0) this.Width = width + 30;
+        }
+
+        // Allow JavaScript to make calls to the app.
+        public void loadHandler(object sender, EventArgs e)
+        {
+            webBrowser1.ObjectForScripting = this;
+        }
+
+    }
+}
