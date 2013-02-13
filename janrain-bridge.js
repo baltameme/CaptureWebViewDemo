d79dff85754494d21ad1c483c91470b32547d733
Matt Garrett <mgarrett@janrain.com>
2013-02-13 15:59:26 -0800

    Moves janrain bridge js into it's own file.

---
+++ janrain-bridge.js
@@ -0,0 +1,26 @@
+function createJanrainBridge(){
+
+    function bridgeIsEnabled(){
+        return !!(typeof janrainNativeAppBridgeEnabled !== 'undefined' && janrainNativeAppBridgeEnabled);
+    }
+
+    janrain.events.onCaptureLoginSuccess.addHandler(function (result) {
+        if (bridgeIsEnabled() && result.accessToken && !result.oneTime) {
+            window.location = "janrain:accessToken=" + result.accessToken;
+        }
+    });
+
+    for (var e in janrain.events) {
+        if (Object.prototype.hasOwnProperty.call(janrain.events, e)){
+            (function(eventName) {
+                if (typeof janrain.events[eventName].addHandler === 'undefined') return;
+                janrain.events[eventName].addHandler(function() {
+                    var argsUrl = "janrain:" + eventName + "?arguments=" + encodeURIComponent(JSON.stringify(arguments));
+                    if (bridgeIsEnabled()) {
+                        window.location = argsUrl;
+                    }
+                });
+            })(e);
+        }
+    }
+}
